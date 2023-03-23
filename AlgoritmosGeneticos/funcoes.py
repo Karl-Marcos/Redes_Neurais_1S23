40d698667363801ee5e5cd8940d62345c867ca40
Marcos de Cerqueira Leite Pimentel <marcos220057@ilum.cnpem.br>
2023-03-23 16:35:26 -0300

    Experimento 4 algoritmo genético para caixa nao binaria

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -16,6 +16,29 @@ def gene_cb():
     return rd.choice(lista)
 
 
+def gene_cnb(valor_max_caixa):
+    """Gera um gene para o problema das caixas não-binárias
+    Args:
+      valor_max_caixa: número inteiro representando o maior valor possível que
+      pode existir dentro de uma caixa.
+    Return:
+      Um valor entre zero e o valor máximo da caixa.
+    """
+    gene = rd.randint(0, valor_max_caixa)
+    return gene
+
+
+def gene_letra(letras):
+    """Sorteia uma letra
+    Args:
+      letras: lista de letras que podem ser sorteadas
+    Return:
+      Retorna uma letra dentro das possíveis de serem sorteadas
+    """
+    letra = rd.choice(letras)
+    return letra
+
+
 def individuo_cb(n):
     """Gera um individuo para o problemas de caixas
 
@@ -33,6 +56,40 @@ def individuo_cb(n):
     return individuo
 
 
+def individuo_cnb(n_genes, valor_max_caixa):
+    """Gera um individuo para o problema das caixas não-binárias.
+    Args:
+      n_genes: número de genes do indivíduo.
+      valor_max_caixa: número inteiro representando o maior valor possível que
+      pode existir dentro de uma caixa.
+    Return:
+       Uma lista com n genes. Cada gene é um valor entre zero e
+       `valor_max_caixa`.
+    """
+    individuo = []
+    for i in range(n_genes):
+        gene = gene_cnb(valor_max_caixa)
+        individuo.append(gene)
+    return individuo
+
+
+def individuo_senha(tamanho_senha, letras):
+    """Cria um candidato para o problema da senha
+    Args:
+      tamanho_senha: inteiro representando o tamanho da senha.
+      letras: letras possíveis de serem sorteadas.
+    Return:
+      Lista com n letras
+    """
+
+    candidato = []
+
+    for n in range(tamanho_senha):
+        candidato.append(gene_letra(letras))
+
+    return candidato
+
+
 def populacao_cb(tamanho, n):
     """Cria uma população no problema de caixas binarias
 
@@ -46,6 +103,22 @@ def populacao_cb(tamanho, n):
     populacao = []
     for _ in range(tamanho):
         populacao.append(individuo_cb(n))
+    return populacao
+
+
+def populacao_cnb(tamanho, n_genes, valor_max_caixa):
+    """Cria uma população para o problema das caixas não-binárias.
+    Args:
+      tamanho: tamanho da população.
+      n_genes: número de genes do indivíduo.
+      valor_max_caixa: maior número inteiro possível dentro de uma caixa
+    Returns:
+      Uma lista onde cada item é um indiviuo. Um individuo é uma lista com
+      `n_genes` genes.
+    """
+    populacao = []
+    for _ in range(tamanho):
+        populacao.append(individuo_cnb(n_genes, valor_max_caixa))
     return populacao
 
 
@@ -101,6 +174,21 @@ def mutacao_cb(individuo):
     """
     gene_para_mutar = rd.randint(0, len(individuo) - 1)
     individuo[gene_para_mutar] = gene_cb()
+    return individuo
+
+
+def mutacao_cnb(individuo, valor_max_caixa):
+    """Realiza a mutação de um gene para o problema das caixas não-binárias
+    Args:
+      individuo:
+        uma lista representado um individuo no problema das caixas não-binárias
+      valor_max_caixa:
+        maior número inteiro possível dentro de uma caixa
+    Return:
+      Um individuo com um gene mutado.
+    """
+    gene_a_ser_mutado = rd.randint(0, len(individuo) - 1)
+    individuo[gene_a_ser_mutado] = gene_cnb(valor_max_caixa)
     return individuo
 
 
